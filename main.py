b2183874c3f2aae7b771b3df3afbf086a31cb5b7
Illia Plotnykov <ilyaplotnykov@gmail.com>
2025-07-26 14:17:22 +0300

    qwe

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from currencies_classes.currencies import Currencies
+from datetime import datetime
+from currencies_classes.request_api import RequestAPI
+from file_reader import FileReader as FR
 
+start = datetime(2025, 5, 1)
+end = datetime(2025, 5, 3)
 
-curr1 = Currencies()
-curr1.add_curr()
-curr1.display_curr()
+# requester = RequestAPI(start, end)
+# requester.get_data()
+
+RequestAPI(start,end).get_data()
