7ef02a50031adbe24731a30e40eec577aaabf844
Illia Plotnykov <ilyaplotnykov@gmail.com>
2025-08-27 17:59:21 +0300

    qwe

--- currencies_classes/file_reader.py
+++ currencies_classes/file_reader.py
@@ -3,7 +3,7 @@ import pandas as pd
 class FileReader:
     @staticmethod
     def get_currency_list(): #Створюємо лист валют з ексель файлу
-        df = pd.read_excel('D:\\Programs\\all_python_projects\\Final_homework_git\\config\\list_currencies.xlsx')
+        df = pd.read_excel('D:\\Programs\\all_python_projects\\Сurrency_Exchange\\config\\list_currencies.xlsx')
         return df['currencies'].tolist()
 
 
